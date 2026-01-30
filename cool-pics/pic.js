e9161391d1cf390278fdb7a6ce11976bbc125aef
THAT-ON3-GUY <raylaser2@gmail.com>
2026-01-30 15:22:58 -0700

    Updated pics site

---
+++ cool-pics/pic.js
@@ -0,0 +1,47 @@
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+const btn = document.querySelector('.menu-btn');
+const menu = document.querySelector('nav');
+
+btn.addEventListener('click', () => {
+    if (window.innerWidth < 1000) { 
+        menu.classList.toggle('show');
+    }
+});
+
+
+function toggleMenu() {
+    menu.classList.toggle('show');
+    
+}
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    if (e.target.tagName !== 'IMG') return;
+    
+    const img = e.target;
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+    const full = src.replace('sm', 'full');
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+    
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
